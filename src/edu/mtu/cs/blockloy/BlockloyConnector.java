f4d1e982c953cb3ea2bee3fb020a4c75b8235d64
SamV-42 <sjvander@mtu.edu>
2022-03-28 01:41:33 -0400

    Analysis engine: multiple instances
    Utter disaster, poorly documented. I'm quite certain how I'm doing it isn't the 'right' way, but it seems to work and count the correct number of instances.
    
    Note: the build process, isn't; I need to reach out to Elijah, because at the moment I manually copied over the directory structure from the existing jar in shakudo repo, and have just been javac/jar'ing/cp-ing it over.

--- src/edu/mtu/cs/blockloy/BlockloyConnector.java
+++ src/edu/mtu/cs/blockloy/BlockloyConnector.java
@@ -16,8 +16,13 @@ import edu.mit.csail.sdg.alloy4compiler.translator.A4Options;
 import edu.mit.csail.sdg.alloy4compiler.translator.A4Solution;
 import edu.mit.csail.sdg.alloy4compiler.translator.TranslateAlloyToKodkod;
 import edu.mit.csail.sdg.alloy4viz.VizGUI;
+import edu.mit.csail.sdg.alloy4.Computer;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public final class BlockloyConnector {
 
@@ -69,17 +74,59 @@ public final class BlockloyConnector {
 		boolean satisfied = false;
 
 		for (Command command: world.getAllCommands()) {
-
 			A4Solution ans = TranslateAlloyToKodkod.execute_command(rep, world.getAllReachableSigs(), command, options);
 
 			if (ans.satisfiable()) {
 
 				String outFilePath = getTempFileLocation();
 				ans.writeXML(outFilePath);
-				new VizGUI(false, outFilePath, null);
+				//try {	System.out.println(new String(Files.readAllBytes(Paths.get(outFilePath))));
+				//} catch(Exception e) { sendErrorToPipe("........"); }
+				//System.out.println("-------");
 
+				class BasicComputer implements Computer {
+
+					A4Solution ans;
+					VizGUI viz;
+					public BasicComputer(A4Solution _ans) {
+						ans = _ans;
+					}
+					public void setViz( VizGUI _viz) {
+						viz = _viz;
+					}
+
+					private String writeout(A4Solution aans) throws Exception {
+						StringWriter out    = new StringWriter();
+						PrintWriter  writer = new PrintWriter(out);
+						aans.writeXML(writer, null, null);
+						writer.flush();
+						return out.toString();
+					}
+
+					@Override public String compute(Object input) throws Exception {
+						ans = ans.next();
+						if(ans.satisfiable()) {
+							String res = writeout(ans);
+							String opath = getTempFileLocation();
+							ans.writeXML(opath);
+							viz.loadXML(opath, true);
+							return writeout(ans);
+						} else {
+							throw new Exception("No more satisfying instances found");
+						}
+					}
+				}
+
+
+				// I genuinely have no idea if this is how it's supposed to be done
+				//   because there's SFA for documentation
+				BasicComputer enumerator = new BasicComputer(ans);
+				BasicComputer evaluator = new BasicComputer(ans);
+				VizGUI viz = new VizGUI(true, outFilePath, null, enumerator, evaluator);
+				enumerator.setViz(viz);
+				evaluator.setViz(viz);
 				satisfied = true;
-
+				//for(A4Solution ayn = ans; ayn.satisfiable(); ayn = ayn.next()) {}
 			}
 
 		}
